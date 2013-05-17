f60a37e157d7767d06dc01b31e6e631661850ca4
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2013-05-17 10:31:57 +0300

    Write() function now working for r.js optimization.

--- template.js
+++ template.js
@@ -1,6 +1,6 @@
 /**
  * RequireJS Template Plugin
- * Copyright 2012 Aurélien Scoubeau (@qur2)
+ * Copyright 2013 Aurélien Scoubeau (@qur2)
  * template.js may be freely distributed under the MIT license.
  */
 (function(global) {
@@ -8,48 +8,45 @@
 // Cache used to map configuration options between load and write
 var buildMap = {};
 
-define(['underscore'], function(_) {
+define(['text', 'underscore'], function(text, _) {
     var template = function(raw) {
-        return _.template(raw);
-    };
+		return _.template(raw);
+	};
 
-    /**
-     * Loads template file as plain text using the text! require plugin
-     * and compiles it with _.template.
-     */
-    template.load = function(name, req, load, config) {
-        // Dojo provides access to the config object through the req function.
-        if (!config) {
-            config = require.rawConfig;
-        }
-        var config = config.template || '';
+	/**
+	 * Loads template file as plain text using the text! require plugin
+	 * and compiles it with _.template.
+	 */
+	template.load = function(name, req, load, config) {
+		var path = name;
+		if (config.isBuild && '/' == name[0]) {
+			path = config.dirBaseUrl + name.substring(1);
+		}
+		text.get(path, function(content) {
+			// Compile the template
+			var compiled = template(content);
+			if (config.isBuild) {
+				// Attach to the build map for use in the write method below.
+				buildMap[name] = compiled;
+			}
+			load(compiled);
+		});
+	};
 
-        var fullName = 'text!' + config.base + name;
+	/**
+	 * Writes the source of the loaded and compiled template as
+	 * a loadable module.
+	 */
+	template.write = function(pluginName, moduleName, write) {
+		var compiled = buildMap[moduleName];
+		write("define(" +
+			"'" + pluginName + "!" + moduleName + "'" +
+			", ['underscore'], function(_) { return " +
+			compiled.source + ";});\n"
+		);
+	};
 
-        // Load the template as plain text
-        req([fullName], function(content) {
-            // compile the template
-            var compiled = template(content);
-            // Attach to the build map for use in the write method below.
-            buildMap[name] = compiled;
-            load(compiled);
-        });
-    };
-
-    /**
-     * Writes the source of the loaded and compiled template as
-     * a loadable module.
-     */
-    template.write = function(pluginName, moduleName, write) {
-        var compiled = buildMap[moduleName];
-        write([
-            "define('", pluginName, "!", moduleName, "', ",
-                "['underscore'], ", compiled.source,
-            ");\n"
-        ].join(""));
-    };
-
-    return template;
+	return template;
 });
 
 })(this);
